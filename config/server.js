d0861acb51129c72f1d576dde9bb4d73400d487c
Rahman <Rahman@DESKTOP-F7BC780>
2024-06-05 16:59:25 +0400

    changes

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "WTNkd8y4TEchnZoFtxyrTg",
+      "Pr6AXDJLSUybzzUShPLtYg",
+      "vAkdQTeZ6Fo+qsQXa3vR5Q",
+      "whcIoe2Cv4i0yJ7zhXHErA",
+    ]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
